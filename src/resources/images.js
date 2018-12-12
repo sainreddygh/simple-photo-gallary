71ad1784c7b9e9222b084e0fe3ffadf2a07e6094
sainreddygh <nsainath.bits@gmail.com>
2018-12-11 22:49:57 -0800

    Adding basic flags in homepage

---
+++ src/resources/images.js
@@ -0,0 +1,8 @@
+export default [   
+    {caption:"caption1", url:"https://www.gettyimages.com/gi-resources/images/Embed/new/embed2.jpg"},
+    {caption:"caption2", url:"https://i.kinja-img.com/gawker-media/image/upload/s--G-sdBgNR--/c_scale,f_auto,fl_progressive,q_80,w_800/z7jcryloxjedsztssw39.jpg"},
+    {caption:"caption3", url:"https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"},
+    {caption:"caption4", url:"https://cdn.atwilltech.com/flowerdatabase/s/sweetly-scented-bouquet-of-flowers-VA02810.425.jpg"},
+    {caption:"caption5", url:"https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"}
+]
+      
